cbdfe81a4daee552707370aca048db0778d0fc32
Conjuction <suhinin-d@mail.ru>
2023-06-16 14:35:58 +0500

    попытка не пытка

--- src/test/java/DemoSberHealth.java
+++ src/test/java/DemoSberHealth.java
@@ -10,6 +10,9 @@ public class DemoSberHealth {
         open("https://sberhealth.ru/");
         $(".the-header__links").find(byText("Онлайн-консультации")).click();
         $(".Title__xaxpyys").shouldHave(text("Срочные консультации"));
+       // $(".Input__c1m3wrv4").click();
+        //$(".Input__iaxpyys").setValue("Аллерголог").pressEnter();
+       // $(".d1sc2n3k__20230615083111 a").shouldHave(text("Аллерголог"));
     }
 
     @Test
@@ -17,5 +20,10 @@ public class DemoSberHealth {
         open("https://sberhealth.ru/");
         $(".the-header__links").find(byText("Приём врача в клинике")).click();
         $(".top-seo-text__title").shouldHave(text("Запись на приём к лучшим врачам Москвы"));
+        //$("#autocomplete__placeholder").click();
+        //$(".v-autocomplete-input").setValue("Реаниматолог").pressEnter();
+        //$(".search-form__button").click();
+        //$(".top-seo-text__title").shouldHave(text("Врачи-реаниматологи"));
+
     }
 }
