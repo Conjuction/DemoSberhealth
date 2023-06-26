25f46a21e7b7fda3f0c63e56ce395ce93262a4aa
Conjuction <suhinin-d@mail.ru>
2023-06-26 19:44:15 +0500

    Добавил параметрезированный тест с выбором услуги

--- src/test/java/DemoSberHealth.java
+++ src/test/java/DemoSberHealth.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -43,7 +44,7 @@ public class DemoSberHealth extends TestBase {
             "Половых органов, Половых органов",
             "Мягких тканей, Мягких тканей"
     })
-    @ParameterizedTest(name = "Проверка перехода по страницам после выбора исследования {0}")
+    @ParameterizedTest(name = "Проверка перехода по страницам после выбора исследования: {0}")
     void checkTitleDiagnostics(String study,
                      String diagnostics) {
         $(".the-header__links").find(byText("Диагностика")).click();
@@ -51,5 +52,22 @@ public class DemoSberHealth extends TestBase {
         $(".organ-top-list").find(byText(study)).click();
         $(".title").shouldHave(text("Виды диагностики " + diagnostics + " в Москве:"));
         $(".logo_link").click();
+    }
+
+    @ValueSource(
+            strings = {"Акушерство", "Аллергология-иммунология", "Анализы", "Андрология", "Вакцинация", "Гастроэнтерология",
+            "Гинекология", "Дерматология", "Кардиология", "Косметология", "ЛОР (отоларингология)", "Маммология",
+            "Массаж и мануальная терапия", "Наркология", "Неврология", "Обследования и диагностики", "Общие процедуры",
+            "Онкология", "Офтальмология", "Пластическая хирургия", "Проктология", "Пульмонология", "Ревматология",
+            "Справки", "Стоматология", "Травматология-ортопедия", "Урология", "Физиотерапия", "Флебология", "Эндокринология"}
+    )
+
+    @ParameterizedTest(name = "Проверка перехода по страницам после выбора услуги: {0}")
+    void checkTitleServices (String service) {
+        $(".the-header__links").find(byText("Услуги")).click();
+        $(".ServiceList__title_4s0e").shouldHave(text("Медицинские услуги в Москве"));
+        $(".service-list__list").find(byText(service)).click();
+        $(".clinic-list-page-header").shouldHave(text(service + " в Москве"));
+        $(".adaptive-header__logo-wrapper").click();
     }
 }
